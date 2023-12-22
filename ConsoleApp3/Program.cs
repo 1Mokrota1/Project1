353f8ba9594b73d8da0a49ff0e77493e0417c27a
1Mokrota1 <154603770+1Mokrota1@users.noreply.github.com>
2023-12-22 17:37:24 +0400

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер таблицы умножения:");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Таблица умножения {number}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
